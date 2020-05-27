24c97d143ded0361848439ebcee5ae9d95d34619
Franklyn Kleiton <franklynkleiton18@gmail.com>
2020-05-27 15:46:42 -0300

    :art: Adding ActionCreators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,35 @@
+// Action Types
 export const ADD_TODO = 'ADD_TODO'
 
 export const TOGGLE_TODO = 'TOGGLE_TODO'
 
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
 
+// Filter's Constants
 export const VisibilityFilters = {
   SHOW_ALL: 'SHOW_ALL',
   SHOW_ACTIVE: 'SHOW_ACTIVE',
   SHOW_COMPLETED: 'SHOW_COMPLETED'
 }
+
+// Action Creators
+export const addTodo = text => (
+  {
+    type: ADD_TODO,
+    text,
+  }
+)
+
+export const toggleTodo = id => (
+  {
+    type: TOGGLE_TODO,
+    id
+  }
+)
+
+export const setVisibilityFilter = filter => (
+  {
+    type: SET_VISIBILITY_FILTER,
+    filter
+  }
+)
