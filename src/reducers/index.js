4431083602b6525388d5327af97090545576e4fb
Franklyn Kleiton <franklynkleiton18@gmail.com>
2020-05-28 13:39:47 -0300

    :recycle: Change Reducer Visibility Filter's Name

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -31,7 +31,7 @@ export const todos = (state = [], action) => {
   }
 }
 
-export const VisibilityFilter = (state = SHOW_ALL, action) => {
+export const visibilityFilter = (state = SHOW_ALL, action) => {
   switch (action.type) {
     case SET_VISIBILITY_FILTER:
       return action.filter
